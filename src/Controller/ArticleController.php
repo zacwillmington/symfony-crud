d02156906bb97e3634ed9768f49072a103a04ec0
zacwillmington <zac.willmington@gmail.com>
2019-07-02 18:18:27 -0500

    Require Sension bundle for route specificity

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,11 +3,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route; // This(Annotations) means that you can define the routes in this file. There is no need to add the routes to the routing.ymal file.
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method; //Adds @method type to route line 12  
 
 class ArticleController {
 
     /**
      * @Route("/", name="Home")
+     * @Method({"GET"})
      */
 
     public function index() {
