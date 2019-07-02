995984b55208394339a8a63a6fffdb414d057fbe
zacwillmington <zac.willmington@gmail.com>
2019-07-02 18:25:20 -0500

    Required twig templating and extended base.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route; // This(Annotations) means that you can define the routes in this file. There is no need to add the routes to the routing.ymal file.
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method; //Adds @method type to route line 12  
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController {
+class ArticleController extends Controller {
 
     /**
      * @Route("/", name="Home")
@@ -13,6 +14,8 @@ class ArticleController {
      */
 
     public function index() {
-        return new Response('<html><body>Hello</body></html>');
+        // return new Response('<html><body>Hello</body></html>');
+        return $this->render('articles/index.html.twig');
+
     }
 }
