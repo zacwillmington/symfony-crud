f2aa474c0511f217ff35b4ddb7ca8a71f82d0213
zacwillmington <zac.willmington@gmail.com>
2019-07-09 17:57:35 -0500

    Made entity Article's and added properties. Ran migration.

---
+++ src/Entity/Article.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text", length = 100)
+     */
+     private $title;
+
+     /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+}
