052e0403b7c475882b2ae617a50788b95328e034
zacwillmington <zac.willmington@gmail.com>
2019-07-10 14:04:56 -0500

    Added show controller and show view file.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -16,13 +16,24 @@ class ArticleController extends Controller {
      */
 
     public function index() {
-        
-        $articles = ['Article one', 'Article Two', 'Article three'];
+
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', array("articles" => $articles));
     }
 
-      /**
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+
+     public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', array("article" => $article));
+     }
+
+
+    /**
      * @Route("/article/save")
      */
 
@@ -30,7 +41,7 @@ class ArticleController extends Controller {
          $entityManager = $this->getDoctrine()->getManager();
 
          $article = new Article();
-         $article->setTitle('Article One');
+         $article->setTitle('Article Two');
          $article->setBody('This is the body');
 
          $entityManager->persist($article);
